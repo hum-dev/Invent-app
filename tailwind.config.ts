1ded75f82041a7cc4e7b71aedf3925e94c60d050
hum-dev <humphreymuriungi@outlook.com>
2024-09-01 13:41:36 +0300

    Added the sidebar and the Navbr to my dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,64 @@
 import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors"
+import colors, { black } from "tailwindcss/colors";
+import { create } from "domain";
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+const generateThemeObject = (colors: any, mapping: any, invert = false) => {
+  const theme: any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    }
+    );
+  }
+  );
+  return theme;
+};
+
+const LightTheme = generateThemeObject(colors, shadeMapping);
+const DarkTheme = generateThemeObject(colors, shadeMapping, true);
+
+const themes = {
+  light: {
+    ...LightTheme,
+    white: "ffffff",
+  },
+  dark: {
+    ...DarkTheme,
+    white: colors.gray["950"],
+    black: colors.gray["50"],
+  },
+  }
+
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +73,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [createThemes(themes)],
 };
 export default config;
